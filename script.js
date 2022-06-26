2d3537cf4485061ae1388df3375e14a9a496cf40
Alexander Pashkov <scias300@gmail.com>
2022-06-26 16:37:15 +0300

    выполнил задание урока 15

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.addElement = function () {
+        let element;
+        if (selector[0] === '.') {
+            element = document.createElement('div');
+            element.classList.add(`${selector.slice(1)}`);
+            element.textContent = 'Новый элемент div ' + `"${element.getAttribute('class')}"`;
+        }
+        if (selector[0] === '#') {
+            element = document.createElement('p');
+            element.setAttribute('id', `${selector.slice(1)}`);
+            element.textContent = 'Новый элемент p ' + `"${element.getAttribute('id')}"`;
+        }
+        element.style.cssText = `height: ${height};
+                width: ${width};
+                background: ${bg};
+                font-size: ${fontSize};
+            `;
+        document.body.append(element);
+    };
+};
+
+const element1 = new DomElement('.class1', '100px', '10px', '', '20px');
+const element2 = new DomElement('#asdasdasd');
+element1.addElement();
+element2.addElement();
